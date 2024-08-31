7c877d913ba1092910f3ea5f6af29cbcaa8602a4
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-08-31 03:30:29 -0600

    Add route POST to create a post

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const router = express.Router();
+const postController = require("../controllers/post.controller");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Post: ' + process.env.ENV);
-});
+router.post(
+  "/",
+  [
+    body("title")
+      .isString()
+      .isLength({ min: 1 })
+      .withMessage("Title is Required"),
+    body("image").isURL().withMessage("Invalid Image URL"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+  postController.createPostController
+);
 
 module.exports = router;

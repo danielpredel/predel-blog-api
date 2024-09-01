57d29b9597cb9a733f6fd00e0b61dbd08ad3468c
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-01 15:04:54 -0600

    Add function to get public Posts (not hidden)

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,10 +1,16 @@
 const Post = require("../models/post.model");
+const { getCurrentDate } = require("../utils/date.utils");
 
 const createPost = (title, image) => {
   const post = new Post();
   post.title = title;
   post.image = image;
   post.hidden = true;
+  //
+  post.author = "Daniel Preciado Delgadillo";
+  post.body = [];
+  post.creationDate = getCurrentDate();
+
   return post
     .save()
     .then((savedPost) => {
@@ -15,6 +21,29 @@ const createPost = (title, image) => {
     });
 };
 
+// All public posts
+const getPosts = async () => {
+  try {
+    const posts = await Post.find(
+      { hidden: true },
+      { _id: 1, title: 1, author: 1, publishDate: 1, image: 1 }
+    );
+    return posts;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// All user's posts
+const getUserPosts = (userId) => {};
+
+// A single public post
+const getPost = (postId) => {};
+
+// A user's post
+const getUserPost = (postId) => {};
+
 module.exports = {
   createPost,
+  getPosts,
 };

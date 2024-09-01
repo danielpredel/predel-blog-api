ae6309c790910520501f59053e74a4adf97736af
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-01 15:09:31 -0600

    Create endpoint GET / in posts, with two different middlewares and controller, in case theres a token or not

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,6 +3,8 @@ const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const postController = require("../controllers/post.controller");
 
+// Create a post with title and image
+// Return the post's ID
 router.post(
   "/",
   [
@@ -19,7 +21,38 @@ router.post(
     }
     next();
   },
-  postController.createPostController
+  postController.createPost
 );
+
+router.get("/", (req, res, next) => {
+  const header = req.headers["authorization"] || "";
+  const token = header.split(" ")[1];
+  if (token) {
+    // try {
+    //   const payload = jwt.verify(token, secretKey);
+    //   req.username = payload.username;
+    //   next();
+    // } catch (error) {
+    //   return res.status(403).json({
+    //     success: false,
+    //     message: "Token ivalido",
+    //     error: "INVALID_TOKEN",
+    //   });
+    // }
+  } else {
+    postController.getPosts(req, res);
+  }
+  // try {
+  //   const payload = jwt.verify(token, secretKey);
+  //   req.username = payload.username;
+  //   next();
+  // } catch (error) {
+  //   return res.status(403).json({
+  //     success: false,
+  //     message: "Token ivalido",
+  //     error: "INVALID_TOKEN",
+  //   });
+  // }
+});
 
 module.exports = router;

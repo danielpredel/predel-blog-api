b81b4a5bcfd0129beaaea6d3a40803a9663088e1
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-03 00:03:22 -0600

    Add cypher password, create token and verification code fuctions

---
+++ src/utils/auth.utils.js
@@ -0,0 +1,42 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+
+// Cypher Password
+const cypherPassword = (password) => {
+  return bcrypt
+    .genSalt(saltRounds)
+    .then((salt) => bcrypt.hash(password, salt))
+    .then((hash) => {
+      return hash;
+    })
+    .catch((err) => {
+      throw new Error(
+        "An error ocurred while hashing the password: " + err?.toString()
+      );
+    });
+};
+
+// Compare Password
+
+// Create Token
+const createToken = (payload) => {
+  const options = {
+    expiresIn: "1h",
+  };
+
+  const token = jwt.sign(payload, process.env.SECRET_KEY, options);
+  return token;
+};
+
+// Create Validation Code
+function createVerificationCode() {
+  return uuidv4();
+}
+
+module.exports = {
+  cypherPassword,
+  createVerificationCode,
+  createToken,
+};

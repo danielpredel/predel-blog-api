7a0c47fb60d6be7761016e6ade9a64ffca7d6189
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-04 10:46:30 -0600

    Declare more users endpoints and include users-post router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const validationMiddleware = require("../middlewares/validation.middleware");
 const userController = require("../controllers/user.controller");
+const userPostsRouter = require("./user.post.routes");
 const router = express.Router();
 
+// Create new user (Sign Up)
 router.post(
   "/",
   validationMiddleware.newUser,
@@ -10,9 +12,15 @@ router.post(
   userController.createUser
 );
 
-router.get(
-  "/check-email",
-  userController.checkEmail
-);
+// Verify email's availability
+router.get("/check-email", userController.checkEmail);
+
+// Send verification email
+router.get("/request-verification-email");
+
+// Validate email
+router.post("/verify-email");
+
+router.use("/posts", userPostsRouter);
 
 module.exports = router;

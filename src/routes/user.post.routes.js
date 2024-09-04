ea559fec43e398089a8c43b0cf49a0c3c163f796
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-04 10:51:04 -0600

    Create users-posts router and declare enpoints. Place POST users/posts enpoint in this file

---
+++ src/routes/user.post.routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const validationMiddleware = require("../middlewares/validation.middleware");
+const postController = require("../controllers/post.controller");
+const router = express.Router();
+
+// Create a new post
+router.post(
+  "/",
+  validationMiddleware.newPost,
+  validationMiddleware.errors,
+  postController.createPost
+);
+
+// Return all users's posts
+router.get("/");
+
+// Return a post if it exists and belongs to the user
+router.get("/:id");
+
+// Create a new post
+router.post(
+  "/",
+  validationMiddleware.newPost,
+  validationMiddleware.errors,
+  postController.createPost
+);
+
+// Edits a post if it exists and belongs to the user
+router.patch("/:id");
+
+// Deltes a posts if it exists and belongs to the user
+router.delete("/:id");
+
+module.exports = router;

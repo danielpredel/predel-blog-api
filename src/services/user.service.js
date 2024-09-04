236cfcd33b24637ce45b1cf33c369b08a9b8c686
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-03 23:12:14 -0600

    Add check email function

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -22,18 +22,29 @@ const createUser = (name, lastname, image, email, password) => {
         id: savedUser.id,
         verified: savedUser.verified,
       });
-      // Create token
-      // Send email with verification code
-      return { token, image: savedUser.image, verified: savedUser.verified };
-      // return { userId: savedUser.id, image: savedUser.image };
+      return {
+        token,
+        image: savedUser.image,
+        verified: savedUser.verified,
+      };
     })
     .catch((err) => {
-      throw new Error(
-        "An error ocurred while creating a new user: " + err?.toString()
-      );
+      throw new Error("An error ocurred while creating a new user: " + err);
     });
+};
+
+const checkEmail = async (email) => {
+  try {
+    const user = await User.findOne({ email });
+    return user;
+  } catch (err) {
+    throw new Error(
+      "An error ocurred while checking an email address: " + err?.toString()
+    );
+  }
 };
 
 module.exports = {
   createUser,
+  checkEmail,
 };

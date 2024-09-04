a7ecd6cd99c2cd2dced88ec2de73191763a88e93
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-03 23:16:32 -0600

    Add check email controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,16 +12,39 @@ const createUser = (req, res) => {
       });
     })
     .catch((err) => {
-      return res
-        .status(500)
-        .json({
-          success: false,
-          errors: err,
-          message: "Something went wrong while creating the user",
+      res.status(500).json({
+        success: false,
+        errors: err,
+        message: "Something went wrong while creating the user",
+      });
+    });
+};
+
+const checkEmail = (req, res) => {
+  const { email } = req.query;
+  userService
+    .checkEmail(email)
+    .then((user) => {
+      if (user) {
+        res.status(200).json({
+          success: true,
+          available: false,
         });
+      } else {
+        res.status(200).json({
+          success: true,
+          available: true,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+      });
     });
 };
 
 module.exports = {
   createUser,
+  checkEmail,
 };

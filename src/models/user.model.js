ac0b69c8e440ccd07e8bf993ce52a80706174af3
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-06 15:56:49 -0600

    Update post user model, remove posts attribute

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -7,7 +7,6 @@ const userSchema = new Schema({
   image: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  posts: { type: [String], default: [] },
   verified: { type: Boolean, default: false },
   verificationCode: { type: String, required: true },
 });

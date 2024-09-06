5d49f20b534b087bdee92c138c9154cd7a035b68
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-05 20:12:24 -0600

    Add endpoint /login in /auth router

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const validationMiddleware = require("../middlewares/validation.middleware");
+const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post(
   "/login",
+  validationMiddleware.login,
+  validationMiddleware.errors,
+  authController.login
 );
 
 module.exports = router;

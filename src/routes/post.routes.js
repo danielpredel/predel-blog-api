6624dfe493b722f6b7b285f37b6b417c61ebc2e1
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-06 19:30:07 -0600

    Delete unecesary code from endpoint GET /posts

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,39 +1,9 @@
 const express = require("express");
-const validationMiddleware = require("../middlewares/validation.middleware");
 const router = express.Router();
 const postController = require("../controllers/post.controller");
 
 // Return public posts
-router.get("/", (req, res, next) => {
-  const header = req.headers["authorization"] || "";
-  const token = header.split(" ")[1];
-  if (token) {
-    // try {
-    //   const payload = jwt.verify(token, secretKey);
-    //   req.username = payload.username;
-    //   next();
-    // } catch (error) {
-    //   return res.status(403).json({
-    //     success: false,
-    //     message: "Token ivalido",
-    //     error: "INVALID_TOKEN",
-    //   });
-    // }
-  } else {
-    postController.getPosts(req, res);
-  }
-  // try {
-  //   const payload = jwt.verify(token, secretKey);
-  //   req.username = payload.username;
-  //   next();
-  // } catch (error) {
-  //   return res.status(403).json({
-  //     success: false,
-  //     message: "Token ivalido",
-  //     error: "INVALID_TOKEN",
-  //   });
-  // }
-});
+router.get("/", postController.getPosts);
 
 // Return public post if it exists
 router.get("/:id");

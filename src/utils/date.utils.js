9dd0349b02e69fcf086ec406404784c96c7ee01b
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-08 12:57:10 -0600

    Add function to get the date in string format (English)

--- src/utils/date.utils.js
+++ src/utils/date.utils.js
@@ -4,6 +4,12 @@ const getCurrentDate = () => {
   return currentDate.toLocaleDateString("en-US", options);
 };
 
+const getStringDate = (date) => {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return date.toLocaleDateString("en-US", options);
+};
+
 module.exports = {
   getCurrentDate,
+  getStringDate,
 };
